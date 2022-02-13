79006854ac5ded6499910e6743f43c9e8eb4d409
Danny O <dannywo@gmail.com>
2022-02-13 14:23:45 -0800

    Completes uniquie chars, using hash table

---
+++ sample-collection/arrays-and-strings/is-unique.js
@@ -0,0 +1,28 @@
+//Implement an algorithm to determine if a string has all unique characters.
+//What if you cannot use additional data structures?
+
+const isUniqueChars = (str) => {
+    //Hash list
+    // create an big array w/ ASCII capacity length 
+    let hashArr = new Array(128);
+    //alternatively, in JS we don't need to set the array length.
+    hashArr = [];
+    console.log(hashArr);
+
+    for (let i = 0; i < str.length; i++) {
+        const val = str.charCodeAt(i);
+        console.log(val);
+        if (hashArr[val]) {
+            return false
+        }
+
+        hashArr[val] = true;
+    }
+
+    return true;
+}
+
+
+console.log(isUniqueChars("Hola"));
+console.log(isUniqueChars("Hhello"));
+console.log(isUniqueChars("uncopyrightable"));
